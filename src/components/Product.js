289eb6d329e4645efb8940ad18d44b89e932c4d0
JulieCode <julieazello@gmail.com>
2023-01-20 12:42:06 -0500

    Run Deployment

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,23 +6,27 @@ import Rating from './Rating'
 const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
-        <Link to={`/product/${product._id}`}>
-            <Card.Img src={product.image} variant='top' />
-        </Link>
+      <Link to={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant='top' />
+      </Link>
 
-        <Card.Body>
+      <Card.Body>
         <Link to={`/product/${product._id}`}>
-            <Card.Title as='div'><strong>{product.name}</strong></Card.Title>.Title>
+          <Card.Title as='div'>
+            <strong>{product.name}</strong>
+          </Card.Title>
         </Link>
 
         <Card.Text as='div'>
-            <Rating value={product.rating} text={`${product.numReviews} reviews`}
-            />
+          <Rating
+            value={product.rating}
+            text={`${product.numReviews} reviews`}
+          />
         </Card.Text>
 
         <Card.Text as='h3'>${product.price}</Card.Text>
-        </Card.Body>
-        </Card>
+      </Card.Body>
+    </Card>
   )
 }
 
